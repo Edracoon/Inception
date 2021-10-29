ec99f7072ffc6932b4f73520b0a179b1964ad3ee
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-10-29 16:51:01 +0200

    wordpress works with nginx, working on mariadb dockerfile

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ all:
 		cd srcs && docker-compose up --build
 down:
 		cd srcs && docker-compose down
+		rm -rf volume_test/mysql/*
+		rm -rf volume_test/wordpress_nginx/*
 
 clean:
 		docker system prune -a -f
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,31 +1,29 @@
 <?php
-define( 'DB_NAME', 'db');
+define( 'DB_NAME', 'db_name');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'user');
+define('DB_USER', 'user_name');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'pass');
+define('DB_PASSWORD', 'user_pass');
 
 define('DB_HOST', 'mariadb:3306');
 define('DB_CHARSET', 'utf8');
-define( 'WP_SITEURL', 'localhost' );
-define( 'WP_HOME', 'localhost' );
+define('WP_SITEURL', 'localhost' );
+define('WP_HOME', 'localhost' );
 
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '' );
 
-
-define('AUTH_KEY',         'mEU]L68@,kS||47_yPKX@xsmO76{,DLCbiEyomB]TePB/uL,buzPrk94qyx*[1XV');
-define('SECURE_AUTH_KEY',  'ql+EK$S,ZH9/8@0P?D5I}0Zq|=1pCv_+!vuHZ5:{|-$Mg<+IYW%t?/[d~<dxQ&fa');
-define('LOGGED_IN_KEY',    'J-6I|LLSx,T/Grgm^(/wfi@;F/g$|v:m+j`a~d{Ix@IY*0`SbT}z^|f3[< 50q-C');
-define('NONCE_KEY',        'xfRq6Tj_,nR@w$UWPp6<K-IS)A/Q%/ebFz:Xi)r7#AN:O^cC/HQLy]$FcOj2*f0o');
-define('AUTH_SALT',        '_{!0$}H-ETI;SG~K2/9qFm-|wqZl=R%R.3Tgguk=+q8[q 2EAY&Z[EP(m`,G[?wp');
-define('SECURE_AUTH_SALT', ':.V~v 7#2^a^wV]v}imcqMJ~MhRbU24l-W80m&iQIAhe}fuCY^6ACR%JGPzbWvF$');
-define('LOGGED_IN_SALT',   'Ybm21O3JG5{J^nfscsR99|/>Xhg<rHPj6Jbitzq<(H5UYI1 Z!Sy]gd(x}*V/oER');
-define('NONCE_SALT',       '(_7DRIcVMB)%{hqEzSdTk|;W3G+-if`<?nZR|{jLSq)E=%r%KTgoM>?`3livGGvg');
+define('AUTH_KEY',         'zrgec5g:uXDG0q(<aru=<=BRlb6*yu}bw&&B$Npk$^Yl4S_593yxglF<WYSd?yP9');
+define('SECURE_AUTH_KEY',  ':?a/k$^Ju.;/Fg;>iJQaKF>|n1[P!&!-i8Xc|=&,(&00ph1l+<A}S% tLN2Z,[NO');
+define('LOGGED_IN_KEY',    'nPu}r0=1)T$H--W?+0W<?>^6ys+LwP~=Zlb{Y*`:dp3kmYf>?mV@x|cxE0)gUC3G');
+define('NONCE_KEY',        '#O}OrAJpw7(UhMSECh+n6Ld~._0YGeE:pcnO@O;yLx--{YFs+IDLQC06?i`VH,9M');
+define('AUTH_SALT',        'wkrhZ)DX7zWeC[qey})qc6J%DMa6@j-p%6Mo?;R4*5SDi5,1nsug/6pCOW]?Iqrm');
+define('SECURE_AUTH_SALT', 'y6yf__gH-=.+*$VK2Pe_auVEc.7jJ)L5sJq`vQ]{-x+ATiNQet(+#Y)Dv^;G1^<l');
+define('LOGGED_IN_SALT',   'i; My*8G&4P0C:S/=u0B^&G[0j*rpq5w@V(S=Y>${D6_*``(-shm~aNls|qd%8$P');
+define('NONCE_SALT',       'pr:AOva;MB!h`|}#|H.6j*ik:StMw+Mmgz($}%Q^:tRdQq hs|$j@-jXZ8R(-{l&');
 define('CONCATENATE_SCRIPTS', false);
 define( 'WP_CACHE_KEY_SALT', 'localhost' );
-
 
 $table_prefix = 'wp_';
 
