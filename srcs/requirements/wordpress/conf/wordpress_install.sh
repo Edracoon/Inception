494bd8ae7f00e13a21dd29d328b46b3407c93bfe
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-11-07 02:39:06 +0100

    oui

--- srcs/requirements/wordpress/conf/wordpress_install.sh
+++ srcs/requirements/wordpress/conf/wordpress_install.sh
@@ -6,16 +6,24 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv -f wp-cli.phar /usr/local/bin/wp
 
+chmod 777 /var/www/html
+
+# moove ma config
+mv /wp-config.php /var/www/html
+
 # Telechargement de WordPress via le CLI (les files de base de Wordpress) en precisant le path
-wp core download --allow-root --path="/var/www/html"
+wp core download --allow-root
+#--path=`/var/www/html`
+
+# Le --path= ne fonctionne pas ? on le fait à la main pour le moment :
+# delete les conf de base
+rm -f wp-config.php
+rm -f wp-config-sample.php
+#moove le content de wordpress dans html
+mv -f wp-* /var/www/html
+mv -f *.php /var/www/html
 
 # On lance l'installation de WordPress (Celle que l'on a normalement en se connectant sur localhost) avec les informations de l'admin
 wp core install --allow-root --url=$SITE_URL --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
-
-
-rm -f /var/www/html/wp-config.php
-rm -f /var/www/html/wp-config-sample.php
-
-mv /wp-config.php /var/www/html
 
 exec php-fpm7 -F
